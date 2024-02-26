4e9c64df5d37e4062131ba17d54534ed62013841
ThomasT00ten <131365613+ThomasT00ten@users.noreply.github.com>
2024-02-26 13:11:09 +0100

    Les 1

---
+++ SlnLes01HerhalingAanvulling/ConsoleComplexiteit/ConsoleComplexiteit/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleComplexiteit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Voer een woord in (druk op Enter om te stoppen): ");
+                string woord = Console.ReadLine().ToLower();
+                // Controleer of het woord leeg is
+                if (woord == string.Empty)
+                {
+                    Console.WriteLine("Bedankt en tot ziens.");
+                    break;
+                }
+
+                int aantalLetters = woord.Length;
+                int aantalLettergrepen = AantalLettergrepen(woord);
+                double complexiteit = Complexiteit(woord, aantalLetters, aantalLettergrepen);
+
+                Console.WriteLine($"de Complexiteit: {complexiteit}");
+                Console.WriteLine($"aantal lettergrepen: {aantalLettergrepen}");
+                Console.WriteLine($"aantal karakters: {aantalLetters}");
+
+            }
+            Console.ReadLine();
+        }
+
+        static bool IsKlinker(char letter)
+        {
+            return "aeiou".IndexOf(letter) != -1;
+        }
+
+        static int AantalLettergrepen(string woord)
+        {
+            int aantalLettergrepen = 0;
+            bool vorigeWasKlinker = false;
+
+            foreach (char letter in woord)
+            {
+                if (IsKlinker(letter) && !vorigeWasKlinker)
+                {
+                    aantalLettergrepen++;
+                }
+
+                vorigeWasKlinker = IsKlinker(letter);
+            }
+
+            return aantalLettergrepen;
+        }
+
+        static double Complexiteit(string woord, int aantalLetters, int aantalLettergrepen)
+        {
+            double complexiteit = aantalLetters / 3.0 + aantalLettergrepen;
+
+            if (woord.Contains('x') || woord.Contains('y') || woord.Contains('q'))
+            {
+                complexiteit++;
+            }
+
+            return Math.Round(complexiteit, 1);
+        }
+    }
+}
+
